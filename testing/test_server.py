490a5a65bf57afda3f7c0b7ad73e3a12cb9f34b0
YehorIv <276801@student.pwr.edu.pl>
2024-10-14 12:54:59 +0200

    Cleanup working directory

--- testing/test_server.py
+++ testing/test_server.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import MagicMock
 from serverapp import *
 from mockserversocket import *
 
@@ -11,12 +12,17 @@ class Logger:
         self.msgs.append(msg)
 
 
+
+
 def test_start1():
+    ss = MagicMock()
+    ss.Accept.return_value = 'fff'
 
     l = Logger()
-    serv = SocketServer(l, MockServerSocket(recvclresult=[None], sendresult=[True]))
+    serv = SocketServer(l, ss)
     serv.AcceptClient()
     assert l.msgs == ["Couldn't recieve client name"]
+
 
 
 def test_start2():
@@ -45,9 +51,14 @@ def test_requests2():
 
 def test_requests3():
     l = Logger()
+    ss = MagicMock()
+    #ss.Recv.return_value = [7, "message", 10, "Hi there!"]
+    ss.Recv.side_effect = []
     serv = SocketServer(l, MockServerSocket(sendresult=[True], recvresult=[7, "message", 10, "Hi there!"]))
     
+    assert serv.messages
     assert serv.ClientProcessRequests("addr") is True
+    ss.Recv.assert_called_once_with()
     assert l.msgs == ['New message from user Bob: Hi there!']
 
 def test_requests4():
